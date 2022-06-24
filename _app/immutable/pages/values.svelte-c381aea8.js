2aca4230ab3d2aad086c3ffceef1ca051bdeff8e
Corrl <70688191+Corrl@users.noreply.github.com>
2022-06-24 21:25:58 +0000

    Deploying to gh-pages from @ Corrl/jscraftcamp_website_svelte-kit@2865bf50f8c46264036513dfb197e9669f65c33a 🚀

---
+++ _app/immutable/pages/values.svelte-c381aea8.js
@@ -0,0 +1,15 @@
+import{S as E,i as S,s as W,x as q,y as A,z as I,r as J,p as V,C as F,e as m,t as d,k as x,c as h,a as v,h as g,d as a,m as C,b as _,g as c,F as y,n as j}from"../chunks/index-ddf8e57f.js";import{P as B}from"../chunks/Page-93f15020.js";function D($){let t,r,n,o,p,f,i,b,u,l,w;return{c(){t=m("p"),r=d(`We are committed to providing a friendly, safe and welcoming environment
+        for all, regardless of gender, sexual orientation, programming language,
+        ability, ethnicity, socioeconomic status, and religion (or lack
+        thereof).`),n=x(),o=m("p"),p=d(`We believe in people and our guests - so we ask nothing more of
+        everybody than to be respectful and friendly with one another.`),f=x(),i=m("p"),b=d(`And so we invite all those who participate in the JSCraftCamp and the
+        community surrounding the conference to help us create safe and positive
+        experiences for everyone.`),u=x(),l=m("p"),w=d(`If you have any questions or problems regarding this Code of Conduct,
+        don't hesitate to contact one of us from the team.`),this.h()},l(e){t=h(e,"P",{class:!0});var s=v(t);r=g(s,`We are committed to providing a friendly, safe and welcoming environment
+        for all, regardless of gender, sexual orientation, programming language,
+        ability, ethnicity, socioeconomic status, and religion (or lack
+        thereof).`),s.forEach(a),n=C(e),o=h(e,"P",{class:!0});var z=v(o);p=g(z,`We believe in people and our guests - so we ask nothing more of
+        everybody than to be respectful and friendly with one another.`),z.forEach(a),f=C(e),i=h(e,"P",{class:!0});var P=v(i);b=g(P,`And so we invite all those who participate in the JSCraftCamp and the
+        community surrounding the conference to help us create safe and positive
+        experiences for everyone.`),P.forEach(a),u=C(e),l=h(e,"P",{class:!0});var k=v(l);w=g(k,`If you have any questions or problems regarding this Code of Conduct,
+        don't hesitate to contact one of us from the team.`),k.forEach(a),this.h()},h(){_(t,"class","svelte-10p6z3n"),_(o,"class","svelte-10p6z3n"),_(i,"class","svelte-10p6z3n"),_(l,"class","svelte-10p6z3n")},m(e,s){c(e,t,s),y(t,r),c(e,n,s),c(e,o,s),y(o,p),c(e,f,s),c(e,i,s),y(i,b),c(e,u,s),c(e,l,s),y(l,w)},p:j,d(e){e&&a(t),e&&a(n),e&&a(o),e&&a(f),e&&a(i),e&&a(u),e&&a(l)}}}function G($){let t,r;return t=new B({props:{headline:"Code of Conduct",center:!0,$$slots:{default:[D]},$$scope:{ctx:$}}}),{c(){q(t.$$.fragment)},l(n){A(t.$$.fragment,n)},m(n,o){I(t,n,o),r=!0},p(n,[o]){const p={};o&1&&(p.$$scope={dirty:o,ctx:n}),t.$set(p)},i(n){r||(J(t.$$.fragment,n),r=!0)},o(n){V(t.$$.fragment,n),r=!1},d(n){F(t,n)}}}function L(){return{stuff:{title:"Values"}}}class M extends E{constructor(t){super(),S(this,t,null,G,W,{})}}export{M as default,L as load};
