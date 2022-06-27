fc6bf72741b43a70c7372c53085f949fad9d9b22
Corrl <github@corinnasteinhoff.de>
2022-06-27 22:14:33 +0200

    impressum

--- src/routes/imprint.svelte
+++ src/routes/imprint.svelte
@@ -10,9 +10,22 @@
 
 <script>
     import Page from "../lib/components/Page.svelte";
+    import Spacer from "../lib/components/Spacer/Spacer.svelte";
 </script>
 
 <Page headline="Impressum">
+
+    <h4>This site is a clone of <a href="https://jscraftcamp.org/">https://jscraftcamp.org/</a></h4>
+    <p>built for self-educational reasons by</p>
+
+    <Spacer size="xxs"/>
+
+    <p>
+        <strong>Corinna Steinhoff</strong><br>
+        Reginbaldstr. 16<br>
+        81247 München<br>
+        jscc@corinnasteinhoff.de
+    </p>
 
 </Page>
 
